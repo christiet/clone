339242f1bb54a501a74cc97cd6f4055b3c3fd037
Tony Christie <tony.christie@gmail.com>
2011-11-11 18:36:42 +0000

    Added application parameters for display

---
+++ prelaunch/config/initializers/application_parameters.rb
@@ -0,0 +1,8 @@
+# Application specific parameters
+
+# using a parameter file rather than ActiveRecord as this is a "one page" application
+# and doesn't need CRUD operations
+
+Prelaunch::Application.config.title = "Prelaunch"
+Prelaunch::Application.config.company = "Seven Brooms"
+Prelaunch::Application.config.tagline = "Extra thin brooms so you can do more tasks ..."
