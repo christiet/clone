5553ebedb7a9a992ff68cee5b4b79e73cf86d2c5
Tony Christie <tony.christie@gmail.com>
2011-11-16 15:15:37 +0000

    basic laypout complete

--- prelaunch/config/initializers/application_parameters.rb
+++ prelaunch/config/initializers/application_parameters.rb
@@ -6,3 +6,4 @@
 Prelaunch::Application.config.title = "Prelaunch"
 Prelaunch::Application.config.company = "SEVEN BROOMS"
 Prelaunch::Application.config.tagline = "EXTRA THIN BROOMS SO YOU CAN DO MORE TASKS AT ONCE ..."
+Prelaunch::Application.config.social = { :twitter => "http://twitter.com/#!/getix6", :gplus => "getix6+", :facebook => "getix6-like", :linkedin => "getix6-in" }
--- prelaunch/spec/controllers/home_controller_spec.rb
+++ prelaunch/spec/controllers/home_controller_spec.rb
@@ -16,48 +16,55 @@ describe HomeController do
   describe "should have the correct layout" do
     render_views
 
-    before(:each) do
-      get 'index'
+    before (:each) do
+      visit 'index'
     end
 
     # check we're displaying all the things we should
-    it "should have a title" do
-      response.should have_selector("title", :content => Prelaunch::Application.config.title)
+    it "has a title" do
+      page.should have_selector("title", :content => Prelaunch::Application.config.title)
     end
 
-    it "should have a company name" do
-      response.should have_selector("#company", :content => Prelaunch::Application.config.company) 
+    it "has a company name" do
+      page.should have_selector("#company", :content => Prelaunch::Application.config.company) 
     end
 
-    it "should have a tagline" do
-      response.should have_selector("#tagline", :content => Prelaunch::Application.config.tagline)
+    it "has a tagline" do
+      page.should have_selector("#tagline", :content => Prelaunch::Application.config.tagline)
     end
 
-    it "should have a logo image" do
-      response.should have_selector("img#logo")
+    it "has a logo image" do
+      page.should have_selector("img#logo")
     end
 
-    it "should have an e-mail capture field" do
-      response.should have_selector("input#email")
+    it "has an e-mail capture field" do
+      page.should have_selector("input#email")
     end
 
-    it "should have an e-mail notify button" do
-      response.should have_selector("a#notify", :href => "#")
+    it "has an e-mail notify button" do
+      page.should have_selector("a#notify", :href => "#")
     end
 
-    pending "should have a follow-us block" do
+    describe "has a follow-us section" do
+      it "has a follow-us block" do
+        page.should have_selector("div#follow")
+      end
 
+      it "has the right elements" do
+        Prelaunch::Application.config.social.map  { |k, v|
+          page.should have_selector("a##{k}", :href => "#{v}")
+        }
+      end
     end
-
   end
 
-  describe "should have a working notify button" do
+  describe "has a working notify button" do
 
-    pending "should validate an e-mail address" do
+    pending "validates an e-mail address" do
 
     end
 
-    pending "should submit an e-mail address to campaign manager" do
+    pending "submits an e-mail address to campaign manager" do
 
     end
   end  
